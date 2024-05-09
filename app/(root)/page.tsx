11d9618269bd65c66d111d2ce4152aef4874423d
Piraharish <piraharish.s@gmail.com>
2024-05-09 23:13:13 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default Home;
